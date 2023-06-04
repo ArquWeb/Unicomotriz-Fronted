3b1aacc4e8a457cbd56c2650790cb98b73b3cfe8
Jorge Juan de Dios <106224225+JorgeJDA@users.noreply.github.com>
2023-06-04 16:07:48 -0500

    landing page
    Se implemento el landing page. Incluyendo los componentes nav-landing y landing. Se trabajo el hmtl y css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavLandingComponent } from './components/nav-landing/nav-landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "landing", component: NavLandingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavLandingComponent } from './components/nav-landing/nav-landing.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './components/shared/angular-material/angular-material.module';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavLandingComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
